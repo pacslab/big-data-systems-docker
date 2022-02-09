8878979b7cd4d599d99719e5fd6e940f04e34f4e
Changyuan Lin <linchangyuan1996@gmail.com>
2022-02-09 20:08:08 +0000

    hadoop

---
+++ hadoop/data/word-count/reducer.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3.9
+
+import sys
+
+previous = None
+sum = 0
+
+for line in sys.stdin:
+    key, value = line.split( '\t' )
+
+    if key != previous:
+        if previous is not None:
+            print(previous + '\t' + str( sum ) )
+
+        previous = key
+        sum = 0
+
+    sum = sum + int( value )
+
+print(previous + '\t' + str( sum ) )
